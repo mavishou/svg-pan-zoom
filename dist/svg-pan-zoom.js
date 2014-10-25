1825cc2f320b49a8699059f25c5d55296d772df0
mavishou <houm@mail.cbi.pku.edu.cn>
2014-10-26 00:04:35 +0800

    move the svg-pan-zoom-control the to up left corner

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -44,7 +44,7 @@ module.exports = {
     // Zoom Group
     var zoomGroup = document.createElementNS(instance.svgNS, 'g');
     zoomGroup.setAttribute('id', 'svg-pan-zoom-controls');
-    zoomGroup.setAttribute('transform', 'translate(' + ( instance.width - 70 ) + ' ' + ( instance.height - 76 ) + ') scale(0.75)');
+    zoomGroup.setAttribute('transform', 'translate(' + 20 + ' ' + 20 + ') scale(0.75)');
     zoomGroup.setAttribute('class', 'svg-pan-zoom-control');
 
     // Control elements
